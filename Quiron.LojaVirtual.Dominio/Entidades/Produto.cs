3891a2ac11a68a11753fed63e5e35608d7dc337f
akemioshiro@hotmail.com <steganos13>
2016-09-19 18:54:07 -0300

    Ajax para a exclusão de produtos

--- Quiron.LojaVirtual.Dominio/Entidades/Produto.cs
+++ Quiron.LojaVirtual.Dominio/Entidades/Produto.cs
@@ -12,10 +12,15 @@ namespace Quiron.LojaVirtual.Dominio.Entidades
     {
         [HiddenInput(DisplayValue =false)]
         public int ProdutoId { get; set; }
+        [Required(ErrorMessage ="Digite o nome do produto")]
         public string Nome { get; set; }
         [DataType(DataType.MultilineText)]
+        [Required(ErrorMessage = "Digite a descrição do produto")]
         public string Descricao { get; set; }
+        [Required(ErrorMessage = "Digite o preço do produto")]
+        [Range(0.01, double.MaxValue, ErrorMessage ="Valor Invalido")]
         public decimal Preco { get; set; }
+        [Required(ErrorMessage = "Digite a categoria do produto")]
         public string Categoria { get; set; }
     }
 }
