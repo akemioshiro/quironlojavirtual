5e846977106ac805ab6cbe1ad30203ccbc422b30
akemioshiro@hotmail.com <steganos13>
2016-09-19 16:33:02 -0300

    Alterar Produto da Area Administrativa

--- Quiron.LojaVirtual.Web/Areas/Administrativo/Controllers/ProdutoController.cs
+++ Quiron.LojaVirtual.Web/Areas/Administrativo/Controllers/ProdutoController.cs
@@ -1,4 +1,5 @@
-﻿using Quiron.LojaVirtual.Dominio.Repositorio;
+﻿using Quiron.LojaVirtual.Dominio.Entidades;
+using Quiron.LojaVirtual.Dominio.Repositorio;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,5 +19,28 @@ namespace Quiron.LojaVirtual.Web.Areas.Administrativo.Controllers
             var produtos = _repositorio.Produtos;
             return View(produtos);
         }
+
+        public ViewResult Alterar (int produtoId)
+        {
+            _repositorio = new ProdutosRepositorio();
+            Produto produto = _repositorio.Produtos.FirstOrDefault(p => p.ProdutoId == produtoId);
+            return View(produto);
+        }
+
+        [HttpPost]
+        public ActionResult Alterar(Produto produto)
+        {
+            if(ModelState.IsValid)
+            {
+                _repositorio = new ProdutosRepositorio();
+                _repositorio.Salvar(produto);
+                
+                TempData["Mensagem"] = string.Format("{0} foi salvo com sucesso!", produto.Nome);
+                return RedirectToAction("index");
+            }
+
+            return View(produto);
+        }
+
     }
 }
